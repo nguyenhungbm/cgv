e69d5b4c7edc1eb8ae5974138106a4d6dabf4126
nguyenhungbm <hungbm@mor.com.vn>
2022-06-17 06:13:39 +0700

    update

--- src/containers/Profile/OrderScreen.vue
+++ src/containers/Profile/OrderScreen.vue
@@ -15,7 +15,7 @@
                 <li id="My-profile" class="sub"><a href="http://localhost:9000/user/profile">Thông tin cá nhân</a></li>
             </ul>
         </li>
-        <li class="item" id="My-Orders"><a href="http://localhost:9000/user/orders?status=0">
+        <li class="item" id="My-Orders"><a href="javascript:;">
             <span>Đơn hàng của tôi</span></a>
             <ul class="item-container">
                 <li id="Returns" class="sub"><a href="http://localhost:9000/user/orders?status=3">Đơn hàng đã nhận</a></li>
@@ -42,34 +42,30 @@
       </div>
       <div class="orders" v-if="list.length" id="orderList">
             <div class="order" v-for="trans in list" :key="trans.id" >
-                  <div class="order-info">
-                     <div class="pull-left">
-                        <div class="info-order-left-text">Đơn hàng <a class="http://localhost:9000/sp/faefae-282"> #29 </a></div>
-                           <p class="text info desc">Đặt lúc 2022-05-14 18:51:22 </p>
-                     </div>
+                <div class="order-info">
+                  <div class="pull-left">
+                    <div class="info-order-left-text">Đơn hàng <a class="http://localhost:9000/sp/faefae-282"> #29 </a></div>
+                    <p class="text info desc">Đặt lúc 2022-05-14 18:51:22 </p>
                   </div>
-                     
-                  <div class="order-item">
-                     <div class="item-pic"><a href="http://localhost:9000/sp/faefae-32"><img src="http://localhost:9000/uploads/2021/01/20/2021-01-20__apple-watch-s3-gps-42mm-vien-nhom-day-cao-su-denct-600x600.png" style="object-fit: contain;"></a></div>
-                     <div class="item-main item-main-mini">
-                        <div class="text title item-title">faefae</div>
-                        <p class="text desc"></p>
-                        <p class="text desc bold"></p>
-                     </div>
-                     <div class="item-quantity"><span class="text desc info multiply">Qty:</span><span class="text">&nbsp;4</span></div>
-                     <div class="label label-primary">
-                        <p class="capsule 
-                                                                         error                         ">Đã hủy</p>
-                     </div>
+                </div>
+                <div class="order-item">
+                  <div class="item-pic"><a href="http://localhost:9000/sp/faefae-32"><img src="http://localhost:9000/uploads/2021/01/20/2021-01-20__apple-watch-s3-gps-42mm-vien-nhom-day-cao-su-denct-600x600.png" style="object-fit: contain;"></a></div>
+                  <div class="item-main item-main-mini">
+                    <div class="text title item-title">faefae</div>
+                    <p class="text desc"></p>
+                    <p class="text desc bold"></p>
                   </div>
-                    
+                  <div class="item-quantity"><span class="text desc info multiply">Qty:</span><span class="text">&nbsp;4</span></div>
+                  <div class="label label-primary">
+                    <p class="capsule error">Đã hủy</p>
+                  </div>
+                </div>
             </div>
-               
-           
          </div>
       </div>
       </div>
    </div>
+    <SpinLoading v-if="isLoading" />
   <FooterLayout />
 </template>
 
@@ -78,12 +74,14 @@ import HeaderLayout from "@/containers/Layout/HeaderLayout.vue";
 import FooterLayout from "@/containers/Layout/FooterLayout.vue";
 import { getOrderDetail } from "@/plugins/api/cart";
 import ListProduct from "@/containers/Component/ListProduct.vue"
-import { ref, onMounted  } from 'vue';
+import SpinLoading from "@/components/SpinLoading";
+import { get, debounce } from "lodash";
 export default {
   name: "OrderScreen",
   components: {
     HeaderLayout,
     FooterLayout,
+    SpinLoading,
     ListProduct
   },
   data() {
@@ -91,32 +89,46 @@ export default {
       transaction : [],
       list : [],
       pagination: null,
+      isLoading: false,
     }
   },
   mounted() {
-    const listElm = document.querySelector('.customer-account-create')
-    listElm.addEventListener('scroll', function(){
-      if (listElm.scrollTop + listElm.clientHeight >= listElm.scrollHeight) {
-        const next_page_url = get(this.pagination, "next_page_url");
-        if (next_page_url) {
-          this.getOrder(next_page_url);
-        }
-      }
-    }) 
+    setTimeout(() => {
+      this.loadMoreOrder();
+    }, 5000);
   },
    created() {
-    const list = this.getOrder();
-    this.list = list;
-  }, 
-  methods: { 
+    this.getOrder();
+  },
+  methods: {
     async getOrder(fullUrl = null){
-      const response = await getOrderDetail(fullUrl);
-      const { data, ...pagination } = response.transaction;
-      console.log(data);
-      this.list = data;
-      this.transaction = response;
-      this.pagination = pagination;
+      try {
+        this.isLoading = true;
+        const response = await getOrderDetail(fullUrl);
+        const { data, ...pagination } = response.transaction;
+        this.list = [...this.list, ...data];
+        console.log(this.list)
+        this.transaction = response;
+        this.pagination = pagination;
+      } catch (error) {
+        this.$toast.error(error.error);
+      } finally {
+        this.isLoading = false;
+      }
     }, 
+    loadMoreOrder() {
+      this.$nextTick(() => {
+        const listElm = document.querySelector('.customer-account-create')
+        listElm.addEventListener('scroll', () => {
+          if (listElm.scrollTop + listElm.clientHeight >= listElm.scrollHeight - 300) {
+            const next_page_url = get(this.pagination, "next_page_url");
+            if (next_page_url) {
+              this.getOrder(next_page_url);
+            }
+          }
+        })
+      })
+    }
   }
 };
 </script>
